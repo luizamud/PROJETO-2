c7d90e11dc691ed804c75caf8fb21a40bf034bb7
Luiz Felipe Moralez Amud <luizamudads@gmail.com>
2019-05-28 22:16:48 -0300

    Manunteçao provisoria de rota para testes

--- routes/member.js
+++ routes/member.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
+
+
     res.render('member');
+
 });
 module.exports = router;
