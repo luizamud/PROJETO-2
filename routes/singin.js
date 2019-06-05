bcbd5c125dd1246d82c23598b4341ed78b039fae
Luiz Felipe Moralez Amud <luizamudads@gmail.com>
2019-06-05 16:51:57 -0300

    fixbug  cadastro , obs: com explicacao de funcionamento

--- routes/singin.js
+++ routes/singin.js
@@ -4,7 +4,7 @@ var client = require('mongodb').MongoClient;
 var url = 'mongodb://127.0.0.1:27017';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('singin');
 });
 router.post('/valid', (req, res) => {
@@ -23,7 +23,40 @@ router.post('/valid', (req, res) => {
         password: senha,
         office: cargo
     };
+    console.log(documento);
+    client.connect(url, { useNewUrlParser: true }, (err, client) => {
+        if (err) throw err;
+        /**
+         * No caso com o mongo e o driver mongo a resposta para se cadastrar e simples,
+         * se nao tiver o registro dentro do banco se responde null ou seja
+         * if(result != null) => Aqui se tiver algo no banco ele devolve uma call back de erro direto pro hbs
+         * if(result == null) => Quer dizer que se nao tiver nada no banco pode cadastrar
+         */
+        if (!err) {
+            var db = client.db('novelmania');
+            db.collection('user').findOne({ username: usuario }, (err, result) => {
+                if (err) throw err;
+                if (!err) {
+                    if(result == null){
+                        console.log("Add banco");
+                       db.collection('user').insertOne(documento,(err)=>{
+                           if(err)throw err;
+                           if(!err){
+                               console.log("Save suceful");
+                           }
+                       });
+                    }
+                    if(result != null){
+                        console.log("Nao pode salvar");
+                        console.log(result);
+                        res.render('singin',{userError: "E-mail ou Usuario ja cadastrados"});
+                    }
 
-   
+                }
+            });
+        }
+    });
+
+
 });
 module.exports = router;
